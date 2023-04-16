4b1fa3d499a6ac05d10cbd018b2e25f2a3e8fe45
Tiaan Broodryk <tiaanbroodryk44@gmail.com>
2023-04-16 20:36:05 +0200

    finished

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -3,50 +3,44 @@ export default function Example() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900">
-            Our offices
+          <h2 className="p-9 text-3xl font-bold tracking-tight text-gray-900">
+            PlasmaTech
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate
-            id malesuada non. Cras aliquet purus dui laoreet diam sed lacus,
-            fames.
+            PlasmaTech is a new company located in Mosselbay that specializes in
+            high-precision plasma cutting. They have a state-of-the-art plasma
+            cutting machine that can cut through any metal quickly and
+            accurately. The plasma cutting machine can cut through steel,
+            aluminum, stainless steel, brass, and other metals with ease. Their
+            highly trained technicians are skilled in using the latest
+            technology and techniques to ensure the highest quality results.
           </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 sm:grid-cols-2 sm:gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           <div>
             <h3 className="border-l border-indigo-600 pl-6 font-semibold text-gray-900">
-              Los Angeles
+              Location
             </h3>
             <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-              <p>4556 Brendan Ferry</p>
-              <p>Los Angeles, CA 90210</p>
+              <p>Mosselbay , Voorbaai</p>
+              <p>Unit2 , 14 Bolton Road</p>
             </address>
           </div>
           <div>
             <h3 className="border-l border-indigo-600 pl-6 font-semibold text-gray-900">
-              New York
+              Contact
             </h3>
             <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-              <p>886 Walter Street</p>
-              <p>New York, NY 12345</p>
+              <p>Nicky</p>
+              <p>076 824 8378</p>
             </address>
           </div>
           <div>
             <h3 className="border-l border-indigo-600 pl-6 font-semibold text-gray-900">
-              Toronto
+              E-Mail
             </h3>
             <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-              <p>7363 Cynthia Pass</p>
-              <p>Toronto, ON N3Y 4H8</p>
-            </address>
-          </div>
-          <div>
-            <h3 className="border-l border-indigo-600 pl-6 font-semibold text-gray-900">
-              London
-            </h3>
-            <address className="border-l border-gray-200 pl-6 pt-2 not-italic text-gray-600">
-              <p>114 Cobble Lane</p>
-              <p>London N1 2EF</p>
+              <p>Nickyvn123@gmail.com</p>
             </address>
           </div>
         </div>
