8a5fd2ced4c9d45d798f3689727204571a02e9c2
Tiaan-Broodryk <tiaanbroodryk44@gmail.com>
2023-04-17 11:19:48 +0200

    delpoyd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+
 import Image from "next/image";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
